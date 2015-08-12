7d80f9624612ad662b395d28fdbd678881412182
Pavol Starek <pavol@starek.sk>
2015-08-12 11:07:32 +0200

    [FIXED #101075036] Objectlink can again delete value now
    Fixed issue with deletion of set objectlink value to empty/unset.
    Linted related files.

--- grunt/configs/eslint.js
+++ grunt/configs/eslint.js
@@ -4,7 +4,7 @@ module.exports = {
 			src: ['src/server/SchemaConstants.js', 'src/server/SchemaTools.js']
 		},
 		client: {
-			src: ['src/client/js/xpsui/filters/**/*.js', 'src/client/js/xpsui/services/form-generator.js']
+			src: ['src/client/js/xpsui/filters/**/*.js', 'src/client/js/xpsui/services/form-generator.js', 'src/client/js/xpsui/directives/objectlink2-edit.js']
 		},
 		tests: {
 			src: ['tests/unit/server/SchemaTools.js']
--- src/client/js/xpsui/directives/objectlink2-edit.js
+++ src/client/js/xpsui/directives/objectlink2-edit.js
@@ -4,23 +4,23 @@
 	angular.module('xpsui:directives')
 	.directive('xpsuiObjectlink2Edit', [
 		'xpsui:logging',
-		'$parse', 
-		'xpsui:DropdownFactory', 
+		'$parse',
+		'xpsui:DropdownFactory',
 		'xpsui:Objectlink2Factory',
-		'xpsui:SelectDataFactory', 
+		'xpsui:SelectDataFactory',
 		'xpsui:SchemaUtil',
 		'xpsui:RemoveButtonFactory',
 	function(log, $parse, dropdownFactory, objectlink2Factory, dataFactory, schemaUtil, removeButtonFactory) {
 		return {
 			restrict: 'A',
 			require: ['ngModel', '?^xpsuiFormControl', 'xpsuiObjectlink2Edit'],
-			controller: function($scope, $element, $attrs) {
-				this.setup = function(){
+			controller: function() {
+				this.setup = function() {
 					this.$input = angular.element('<div tabindex="0"></div>');
 					this.$input.addClass('x-input');
 				};
-				
-				this.getInput = function(){
+
+				this.getInput = function() {
 					return this.$input;
 				};
 
@@ -31,20 +31,19 @@
 				log.group('ObjectLink2 edit Link');
 
 				var ngModel = ctrls[0],
-					formControl = ctrls[1] || {},
 					selfControl = ctrls[2],
 					input = selfControl.getInput(),
 					parseSchemaFragment = $parse(attrs.xpsuiSchema),
 					schemaFragment = parseSchemaFragment(scope)
 				;
 
-				var removeButton = removeButtonFactory.create(elm,{
-					enabled: !!!schemaFragment.required,
+				var removeButton = removeButtonFactory.create(elm, {
+					enabled: !schemaFragment.required,
 					input: input,
-					onClear: function(){
+					onClear: function() {
 						input.empty();
 						scope.$apply(function() {
-							ngModel.$modelValue = {};
+							ngModel.$setViewValue(null);
 						});
 					}
 				});
@@ -53,7 +52,7 @@
 				elm.addClass('x-select-edit x-objectlink2-edit');
 
 				ngModel.$render = function() {
-					if(!angular.equals({},ngModel.$viewValue)) {
+					if(!angular.equals({}, ngModel.$viewValue)) {
 						// get data from schema or model and render it
 						render(dataFactory.getObjectLinkData(
 							schemaFragment.objectLink2, ngModel.$modelValue
@@ -70,8 +69,8 @@
 						removeButton.show();
 						// get fields schema fragments
 						schemaUtil.getFieldsSchemaFragment(
-							schemaFragment.objectLink2.schema, 
-							schemaFragment.objectLink2.fields, 
+							schemaFragment.objectLink2.schema,
+							schemaFragment.objectLink2.fields,
 							function(fields) {
 								objectlink2Factory.renderElement(
 									input,
@@ -86,7 +85,7 @@
 				elm.append(input);
 
 
-				elm.bind('focus', function(evt) {
+				elm.bind('focus', function() {
 					input[0].focus();
 				});
 
@@ -94,15 +93,15 @@
 				var dropdown = dropdownFactory.create(elm, {
 					titleTransCode: schemaFragment.transCode
 				});
-				
+
 				dropdown.setInput(selfControl.getInput())
 					.render()
 				;
 
 				// selectobx
 				var selectbox = objectlink2Factory.create(elm, {
-					onSelected: function(value){
-						
+					onSelected: function(value) {
+
 						scope.$apply(function() {
 							ngModel.$setViewValue(
 								value
