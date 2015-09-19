0f0c24c4e21fbcb0939576a7c733e4854d5e7dc3
Pavol Starek <pavol@starek.sk>
2015-09-19 21:09:22 +0200

    HOTFIX wrong date handling 31th day
    Wrong order of setting date properties caused in months that
    have 30 days components always reset datum to 1th.

--- src/client/js/xpsui/services/date-util.js
+++ src/client/js/xpsui/services/date-util.js
@@ -28,9 +28,9 @@
 					(year > 0 && year < 10000)
 				) {
 					var d = new Date();
-					d.setDate(day);
-					d.setMonth(month -1);
 					d.setYear(year);
+					d.setMonth(month -1);
+					d.setDate(day);
 					return d;
 				}
 			}	
