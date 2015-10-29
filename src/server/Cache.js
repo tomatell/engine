a1159e26abfc8a64515d1a6bd14166a711918b9e
Pavol Starek <pavol@starek.sk>
2015-10-29 11:13:26 +0100

    Documentation of Cache class
    Source code documentation of Cache class.

--- src/server/Cache.js
+++ src/server/Cache.js
@@ -6,6 +6,11 @@
 	var config = require('./config.js');
 	var Q = require('q');
 
+	/**
+	 * Wraps cache functionality.
+	 *
+	 * @class Cache
+	 */
 	var Cache = function(options) {
 		var opts = options || config.defaultRedis;
 		this.redis = new IORedis(opts);
@@ -19,6 +24,17 @@
 		});
 	};
 
+	/**
+	 * Stores key/value pair in cache with possible expiration set.
+	 *
+	 * @method store
+	 * @param {string} key name key
+	 * @param {string} val value
+	 * @param {number} exp defines expiration in seconds if set. If not set
+	 * no expiration is set
+	 *
+	 * @return promise
+	 */
 	Cache.prototype.store = function(key, val, exp) {
 		// continue only if ready
 		if (!this.ready) {
@@ -28,6 +44,14 @@
 		return this.redis.set(key, val, exp ? ['EX', exp] : []);
 	};
 
+	/**
+	 * Returns cached value if found
+	 *
+	 * @method get
+	 * @param {string} key
+	 *
+	 * @return promise that gets cached value as parameter for then call
+	 */
 	Cache.prototype.get = function(key) {
 		if (!this.ready) {
 			return Q.resolve(null);
@@ -36,6 +60,21 @@
 		return this.redis.get(key);
 	};
 
+	/**
+	 * Registers cache key for eviction. This construction is used for
+	 * forced eviction of objects by call of evict all registered. In general
+	 * every key registered by this method is evicted by evictAllRegistered method call.
+	 * Take into consideration that registered keys are hold in cache for indefinite time
+	 * and are removed only by evictAllRegistered call. So putting too many keys here can
+	 * grow list to be pretty big and subsequent evictAllRegistered can take significant time.
+	 *
+	 * @method registerForEviction
+	 * @param {string} evictedBy defines registration group which should evict this key
+	 * @param {string} key to evict
+	 *
+	 * @return promise
+	 *
+	 */
 	Cache.prototype.registerForEviction = function(evictedBy, key) {
 		// continue only if ready
 		if (!this.ready) {
@@ -45,6 +84,16 @@
 		return this.redis.sadd(evictedBy, key);
 	};
 
+	/**
+	 * Removes all keys that are registered by this eviction group.
+	 *
+	 * See {{#crossLink "Cache/registerForEviction"}}{{/crossLink}} for more info.
+	 *
+	 * @method evictAllRegistered
+	 * @param {string} evictedBy name of eviction group
+	 *
+	 * @return promise
+	 */
 	Cache.prototype.evictAllRegistered = function(evictedBy) {
 		// continue only if ready
 		if (!this.ready) {
